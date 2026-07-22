502ea9610081a6454a593ae096e40f0ce5533a2a
Niiko235 <nicolaseplaza@gmail.com>
2026-07-22 12:51:43 -0500

    "create first window with html"

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('Hola mundo')
+// console.log('Hola mundo')
+
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
